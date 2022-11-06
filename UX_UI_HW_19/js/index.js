be9065d6536d4b0e07294840f204a84b66d1cf50
Hollyoclark <106997612+Hollyoclark@users.noreply.github.com>
2022-11-06 12:46:47 -0600

    update

--- UX_UI_HW_19/js/index.js
+++ UX_UI_HW_19/js/index.js
@@ -5,3 +5,43 @@ function mouseOver() {
   function mouseOut() {
     document.getElementById("hover").style.color = "white";
   }
+
+  function changeColor(obj) {
+    if (obj.style.color == 'skyblue') {
+        obj.style.color = 'white';
+    } else {
+        obj.style.color = 'skyblue';
+    }
+}
+ 
+function changeBgColor(obj, colorName) {
+    obj.style.backgroundColor = colorName;
+}
+ 
+function changeText(obj, text) {
+    obj.innerText = text;
+}
+ 
+function scaleUp(obj, size) {
+    obj.style.fontSize = size;
+}
+ 
+function showSix() {
+    var element = document.getElementById("six");
+     
+    if(element.style.visibility == "hidden"){
+        //element.style.display = "block";
+        element.style.visibility = "visible";
+    } else {
+        //element.style.display = "none";
+        element.style.visibility = "hidden";
+    }   
+}
+
+function changeColor2(obj) {
+  if (obj.style.color == 'skyblue') {
+      obj.style.color = 'black';
+  } else {
+      obj.style.color = 'skyblue';
+  }
+}
--- UX_UI_HW_20/js/index.js
+++ UX_UI_HW_20/js/index.js
@@ -7,10 +7,45 @@ function mouseOver() {
     document.getElementsByClassName("hover").style.color = "white";
   }
 
-  navbtn.addEventListener('mouseenter', function() {
+  navbtn.addEventListener('mouseover', function() {
     navbtn.style.color = "red";
   })
 
   document.getElementsByClassName("hover").addEventListener('mouseleave', function() {
     document.getElementsByClassName("hover").style.color = "white";
   })
+
+  
+
+        
+  function changeColor(obj) {
+      if (obj.style.color == 'orange') {
+          obj.style.color = 'white';
+      } else {
+          obj.style.color = 'orange';
+      }
+  }
+   
+  function changeBgColor(obj, colorName) {
+      obj.style.backgroundColor = colorName;
+  }
+   
+  function changeText(obj, text) {
+      obj.innerText = text;
+  }
+   
+  function scaleUp(obj, size) {
+      obj.style.fontSize = size;
+  }
+   
+  function showSix() {
+      var element = document.getElementById("six");
+       
+      if(element.style.visibility == "hidden"){
+          //element.style.display = "block";
+          element.style.visibility = "visible";
+      } else {
+          //element.style.display = "none";
+          element.style.visibility = "hidden";
+      }   
+  }
